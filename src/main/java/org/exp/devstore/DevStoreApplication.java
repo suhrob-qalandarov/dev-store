51603723a68de109cb2b7ad625b320d80994bc88
Suhrob <suhrob_kalandarov@outlook.com>
2025-05-24 17:26:18 +0500

    Initialize Spring Boot project with Maven wrapper
    Added a Spring Boot project structure with Maven wrapper files to simplify build management. Configured `.gitignore`, `pom.xml`, and `application.properties` to set up the project. Included dependencies for Spring Boot, JPA, PostgreSQL, and Lombok.

---
+++ src/main/java/org/exp/devstore/DevStoreApplication.java
@@ -0,0 +1,13 @@
+package org.exp.devstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/exp/devstore/DevStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exp.devstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
